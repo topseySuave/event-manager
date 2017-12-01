de956b1e561a9c25e61b13cebbc7cfbe1854189d
topseySuave <gabrielsuave17@gmail.com>
2017-12-02 00:44:38 +0100

    refactor: config.js to try to fix dialect error

--- server/config/config.js
+++ server/config/config.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 module.exports = {
   secret: '#(@#GYzU#EWERGFXTSWEW(@#YW($EW(',
   development: {
-    username: "postgres",
-    password: "gabmicah",
-    database: "boots",
-    host: "localhost",
-    port: 5432,
-    dialect: "postgres"
+      username: "rbymklmi",
+      password: "zdD4ebEejyZBWRJLqajpqs0c0OT0lfEl",
+      database: "rbymklmi",
+      host: "baasu.db.elephantsql.com",
+      port: 5432,
+      dialect: "postgres"
   },
   test : {
-    username: "postgres",
-    password: "gabmicah",
-    database: "boots",
-    host: "localhost",
-    port: 5342,
-    dialect: "postgres"
+      username: "rbymklmi",
+      password: "zdD4ebEejyZBWRJLqajpqs0c0OT0lfEl",
+      database: "rbymklmi",
+      host: "baasu.db.elephantsql.com",
+      port: 5432,
+      dialect: "postgres"
   },
   production : {
     use_env_variable: process.env.DATABASE_URL_PRODUCTION,
