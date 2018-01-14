f8f6744538ca0cbc1b3c2f9ca285c825e773524a
topseySuave <gabrielsuave17@gmail.com>
2018-01-14 15:14:13 +0100

    feat: modify single event Modal to handle both edit and create events..

---
+++ client/src/components/modals/validateInput.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+import isEmpty from 'lodash/isEmpty';
+
+export function validateEventInput(data) {
+    let errors = {};
+
+    if (isEmpty(data.title)) {
+        errors.title = 'This field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+}
+
+export function validateCenterInput(data) {
+    let errors = {};
+
+    if (isEmpty(data.title)) {
+        errors.title = 'Title field is required';
+    }
+
+    if (isEmpty(data.location)) {
+        errors.location = 'Location field is required';
+    }
+
+    if(isEmpty(data.description)){
+        errors.description = 'Description field is required';
+    }
+
+    if(data.price < 100){
+        errors.price = 'Price should be more than 100';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+}
+
