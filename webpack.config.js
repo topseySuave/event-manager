4d41f7a26ae63666e65314d396bceb1e3fd15dcb
topseySuave <gabrielsuave17@gmail.com>
2018-02-16 11:43:54 +0100

    changes: file rename from js to jsx (from standard react-script)

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devtool: 'eval-source-map',
     entry: [
         'webpack-hot-middleware/client',
-        path.join(__dirname, 'client/src/index.js')
+        path.join(__dirname, 'client/src/index.jsx')
     ],
     output: {
         publicPath: '/',
