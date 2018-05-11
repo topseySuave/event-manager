68b9ba23ffa9eec500371f47678bb20b8f44e039
topseySuave <gabrielsuave17@gmail.com>
2018-05-11 14:33:37 +0100

    fix(status-field): add status field to events table

--- server/models/events.js
+++ server/models/events.js
@@ -5,11 +5,6 @@ const Events = (sequelize, DataTypes) => {
     description: DataTypes.TEXT,
     startDate: DataTypes.DATE,
     endDate: DataTypes.DATE,
-    status: {
-      type: DataTypes.ENUM('pending', 'accepted', 'rejected'),
-      allowNull: false,
-      defaultValue: 'pending'
-    },
     centerId: {
       type: DataTypes.INTEGER,
       references: {
