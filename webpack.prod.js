5cb8e0de0dfd2468d01952de573e9908daac8d51
topseySuave <gabrielsuave17@gmail.com>
2018-05-20 10:39:35 +0100

    fix(webpack): take off uglify plugin package and use only webpack provided uglify plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,9 @@
 const merge = require('webpack-merge'); // eslint-disable-line
 const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   plugins: [
-    new UglifyJSPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       'process.env.SECRET_KEY': JSON.stringify(process.env.SECRET_KEY)
