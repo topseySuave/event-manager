cd85fb03c687d3ccb52f9f62d782294da09f43c1
topseySuave <gabrielsuave17@gmail.com>
2018-05-20 13:19:15 +0100

    fix(webpack): turn source map false

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,16 @@
 const merge = require('webpack-merge'); // eslint-disable-line
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   plugins: [
+    new UglifyJSPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         SECRET_KEY: JSON.stringify(process.env.SECRET_KEY)
       }
     }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        unused: true,
-        dead_code: true, // big one--strip code that will never execute
-        warnings: false, // good for prod apps so users can't peek behind curtain
-        drop_debugger: true,
-        conditionals: true,
-        evaluate: true,
-        drop_console: true, // strips console statements
-        sequences: true,
-        booleans: true,
-      },
-      comments: false,
-      sourceMap: false,
-      minimize: true
-    }),
+    new webpack.optimize.OccurrenceOrderPlugin()
   ],
 });
