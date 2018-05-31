09539664098b79c18aa9259ef46a2f75e43150e2
topseySuave <gabrielsuave17@gmail.com>
2018-05-31 08:07:45 +0100

    chore(): change base url for e2e testing

--- e2eTests/signin.test.js
+++ e2eTests/signin.test.js
@@ -1,5 +1,5 @@
 /* global module */
-const APP_BASE_PATH = 'http://localhost:8000';
+const APP_BASE_PATH = 'http://localhost:8000/signin';
 
 module.exports = {
   'first view to the landing page': (browser) => {
@@ -7,7 +7,7 @@ module.exports = {
       .url(APP_BASE_PATH)
       .waitForElementVisible('body', 5000)
       .assert.urlEquals(`${APP_BASE_PATH}`)
-      .pause(10000)
+      .pause(2000)
       .assert.visible('body')
       .end();
   }
